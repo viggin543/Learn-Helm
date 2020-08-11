2f738944cdde38f1c1ae23667bab20d82ab371ac
Domrev Igor <domrevigor@gmail.com>
2020-08-11 22:35:05 +0300

    my first operator

--- guestbook-operator/cmds.sh
+++ guestbook-operator/cmds.sh
@@ -15,3 +15,14 @@ kubectl apply -f deploy -n gitops
 kubectl apply -f deploy/crds/helm.operator-sdk_v1alpha1_guestbook_cr.yaml # this will cause the operator to install out chart
 
 # modify redis slaves count in guestbook cr and apply to cluster, operator will upgrade the chart
+k apply -f guestbook-operator/deploy/crds/helm.operator-sdk_v1alpha1_guestbook_cr.yaml -n gitops
+
+# . Because the release history is not retained,
+# the Helm-based operator does not allow you to perform an explicit rollback
+# this can be achieved by applying changes to the custom resource values
+
+k delete -f guestbook-operator/deploy/crds/helm.operator-sdk_v1alpha1_guestbook_cr.yaml -n gitops
+k delete pvc -n gitops -l app=redis
+kubectl delete -f deploy -n gitops # remove the operator
+
+
