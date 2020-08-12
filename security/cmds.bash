c512d5d4ccc9e62a27655fb0761e6abed649c123
Domrev Igor <domrevigor@gmail.com>
2020-08-12 11:29:25 +0300

    my first operator vas very fun

---
+++ security/cmds.bash
@@ -0,0 +1,11 @@
+gpg --generate-key
+
+#gpg: directory '/Users/domrevigor/.gnupg/openpgp-revocs.d' created
+#gpg: revocation certificate stored as '/Users/domrevigor/.gnupg/openpgp-revocs.d/9A8D1560FE38B393A7284EE3B8C75AD0E774B5EB.rev'
+#public and secret key created and signed.
+#
+#pub   rsa2048 2020-08-11 [SC] [expires: 2022-08-11]
+#      9A8D1560FE38B393A7284EE3B8C75AD0E774B5EB
+#uid                      domrevigor <domrevigor@gmail.com>
+#sub   rsa2048 2020-08-11 [E] [expires: 2022-08-11]
+
